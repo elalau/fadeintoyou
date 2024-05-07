e58cc758471971bb4f905dc6c92271946ae6b4ad
elalau <145486589+elalau@users.noreply.github.com>
2024-05-06 19:18:37 -0700

    push

---
+++ sketcheight.js
@@ -0,0 +1,50 @@
+var img1; 
+var img2; 
+var stars = [];
+
+function preload() {
+  img1 = loadImage('SOME.png'); 
+  img2 = loadImage('YOUR.png');
+}
+
+function setup() {
+  createCanvas(1200, 800);
+  
+  for (var i = 0; i < 1000; i++) {
+		stars[i] = new Star();
+	}
+}
+
+
+function draw() {
+  background('black');
+  let x = mouseX;
+  let y = mouseY;
+  let ix = width - mouseX;  // Inverse X
+  let iy = height - mouseY; // Inverse Y
+
+
+  image(img1, x, height/6, y , y);
+  image(img2, ix, height/6, iy, iy);
+  
+  for (var i = 0; i < stars.length; i++) {
+		stars[i].draw();
+	}
+}
+
+class Star {
+	constructor() {
+		this.x = random(width);
+		this.y = random(height);
+		this.size = random(0.25, 3);
+		this.t = random(TAU);
+	}
+	
+	draw() {
+		this.t += 0.1;
+		var scale = this.size + sin(this.t) * 2;
+		noStroke();
+		ellipse(this.x, this.y, scale, scale);
+	}
+}
+
